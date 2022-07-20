9e18a417e023a1294f48aa76843b01744cedcdf3
Matthew Kates <kates.matthew@gmail.com>
2022-07-19 21:28:11 -0400

    updated

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -3,11 +3,21 @@
 @section('title', $post['title'])
 
 @section('content')
-
-@if($post['is_new'])
-    <div>A new blog post</div>
-@endif
-
 <h1>{{ $post['title'] }}</h1>
 <p>{{ $post['content'] }}</p>
+
+@if($post['is_new'])
+    <div>A new blog post using if</div>
+@elseif(!$post['is_new'])
+    <div>blog post is old using elseif</div>
+@endif
+
+@unless($post['is_new'])
+    <div>blog post is old using unless</div>
+@endunless
+
+@isset($post['has_comments'])
+    <div>This post has some comments using isset</div>
+@endisset
+
 @endsection
